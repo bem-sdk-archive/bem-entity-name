e7983e3248f81232bd47c87b296074a06cfa926d
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2016-04-04 22:02:00 +0300

    Increase coverage.

--- test/type.test.js
+++ test/type.test.js
@@ -2,14 +2,19 @@ import test from 'ava';
 import sinon from 'sinon';
 import proxyquire from 'proxyquire';
 
-const spy = sinon.spy();
-const BemEntity = proxyquire('../index', {
-    'bem-naming': {
-        typeOf: spy
-    }
+test.beforeEach(t => {
+    t.context.spy = sinon.spy();
+    t.context.BemEntity = proxyquire('../index', {
+        'bem-naming': {
+            typeOf: t.context.spy
+        }
+    });
 });
 
+
 test('should use `naming.typeOf()` for block', t => {
+    const BemEntity = t.context.BemEntity;
+    const spy = t.context.spy;
     const entity = new BemEntity({ block: 'block' });
 
     /*eslint no-unused-expressions: "off"*/
@@ -19,6 +24,8 @@ test('should use `naming.typeOf()` for block', t => {
 });
 
 test('should use `naming.typeOf()` for elem', t => {
+    const BemEntity = t.context.BemEntity;
+    const spy = t.context.spy;
     const entity = new BemEntity({ block: 'block', elem: 'elem' });
 
     /*eslint no-unused-expressions: "off"*/
@@ -28,6 +35,8 @@ test('should use `naming.typeOf()` for elem', t => {
 });
 
 test('should use `naming.typeOf()` for block modifier', t => {
+    const BemEntity = t.context.BemEntity;
+    const spy = t.context.spy;
     const entity = new BemEntity({ block: 'block', modName: 'mod', modVal: 'val' });
 
     /*eslint no-unused-expressions: "off"*/
@@ -37,10 +46,31 @@ test('should use `naming.typeOf()` for block modifier', t => {
 });
 
 test('should use naming.typeOf() for element modifier', t => {
+    const BemEntity = t.context.BemEntity;
+    const spy = t.context.spy;
     const entity = new BemEntity({ block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' });
 
     /*eslint no-unused-expressions: "off"*/
     entity.type;
 
     t.ok(spy.calledWith({ block: 'block', elem: 'elem', modName: 'mod', modVal: 'val' }));
+});
+
+test('should cache type value', t => {
+    const stub = sinon.stub();
+    const BemEntity = proxyquire('../index', {
+        'bem-naming': {
+            typeOf: stub
+        }
+    });
+
+    stub.returns('type');
+    const entity = new BemEntity({ block: 'block' });
+
+    /*eslint no-unused-expressions: "off"*/
+    entity.type;
+    entity.type;
+    entity.type;
+
+    t.is(stub.callCount, 1);
 });
